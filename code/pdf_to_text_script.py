df708659afc63d56f185bb27cf41dc59530db843
Anna Haensch <annahaensch@gmail.com>
2021-11-12 13:18:14 -0500

    add mkdir and rmdir to script

--- code/pdf_to_text_script.py
+++ code/pdf_to_text_script.py
@@ -9,22 +9,26 @@ from pdf2image import pdfinfo_from_path,convert_from_path
 logging.basicConfig(level=logging.INFO)
 
 
-YEAR = 2019 #2020
-PATH_TO_DATA = "data/primary_datasets/Logs{}.pdf".format(YEAR)
-
-
 def main(argv):
+
+    year = argv[0]
+    logging.info("Parsing logs for {}".format(year))
+
+    path = "data/primary_datasets/Logs{}.pdf".format(year)
 
     logging.info("\n Retrieving Pages")
 
-    info = pdfinfo_from_path(PATH_TO_DATA, userpw=None, poppler_path=None)
+    info = pdfinfo_from_path(path, userpw=None, poppler_path=None)
 
     maxPages = info["Pages"]
 
     n = 1
 
+    # Create temporary directory for image files.
+    os.mkdir("data/temp_pdf_to_jpg")
+
     for page in range(1, maxPages+1, 10) :
-        pages = convert_from_path(PATH_TO_DATA, 
+        pages = convert_from_path(path, 
                               dpi=300, 
                               first_page=page, 
                               last_page = min(page+10-1,maxPages),
@@ -35,12 +39,12 @@ def main(argv):
 
         for page in pages:
             # Write pdf to image file.
-            img_file = 'data/{}_jpg_logs/out_{}.jpg'.format(YEAR,n)
+            img_file = 'data/{}_jpg_logs/out_{}.jpg'.format(year,n)
             page.save(img_file.format(n),"JPEG")
 
 
             # Write image file to text file.
-            text_file = open('data/{}_text_logs/page_{}.txt'.format(YEAR,n), 'w')
+            text_file = open('data/{}_text_logs/page_{}.txt'.format(year,n), 'w')
             text=str(pytesseract.image_to_string(Image.open(img_file),lang='eng', config='--psm 12'))  # 12
             text=text.replace("\n"," ") # make spaces instead of lose info
 
@@ -53,6 +57,8 @@ def main(argv):
 
             n += 1
 
+    # Delete temporary directory for image files.
+    os.rmdir("data/pdf_to_jpg")
 
 if __name__ == "__main__":
     main(sys.argv[1:])

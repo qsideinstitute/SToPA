e9b5ba1a6958984a73e93a7080d50e40cdd5463d
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2021-12-26 19:45:57 -0800

    Overhaul of OCR processing with a top-level settings file, switches, and greater logging.

--- code/pdf_to_text_script.py
+++ code/pdf_to_text_script.py
@@ -2,63 +2,134 @@ import logging
 import sys
 import os
 
+# hacky reference to parent directory with settings.
+
+# TODO: decide whether we want project structure to work this way.
+# Alternative is to force users to always interact with the top-level directory 
+# only, and import stuff in the "code" subdirectory rather than running it directly.
+sys.path.append(os.path.abspath('..'))
+import settings
+
 from PIL import Image
-import pytesseract
+pytesseract = settings.pytesseract
 from pdf2image import pdfinfo_from_path,convert_from_path
 
-logging.basicConfig(level=logging.INFO)
+#
+pytesseract.pytesseract.tesseract_cmd = settings.TESSERACT_EXECUTABLE
 
+# TODO: put the level of 5 as a parameter in the settings. (note: this is below logging.DEBUG, at 10)
+VERBOSE_LOGLEVEL = 5    # for logging.log() messages about individual file creation/deletion
+
+logging.basicConfig(level = VERBOSE_LOGLEVEL) # display messages with level (priority) >= level defined here.
+# note by default:
+# logging.DEBUG -> 10
+# logging.INFO -> 20
+# logging.ERROR -> 40
+
+def _soft_mkdir(fname):
+    '''
+    Checks to see if folder exists; if yes, displays warning. Otherwise makes folder.
+    Then returns the input.
+    '''
+    if not os.path.exists(fname):
+        os.mkdir(fname)
+    else:
+        logging.warning("Folder {} already exists; temp files may be overwritten.".format(fname))
+    return
 
 def main(argv):
 
     year = argv[0]
     logging.info("Parsing logs for {}".format(year))
 
-    path = "data/primary_datasets/Logs{}.pdf".format(year)
+    try:
+        pdfpath = settings.PDF_DICT[ year ]
+    except:
+        logging.error("Error finding logs. Expecting data in location:")
+        logging.error("[PROJECT_FOLDER]/data/primary_datasets/Logs{}.pdf\n".format(year))
+        raise Exception("Failed to load logs for input year {}.".format(year))
+    
 
     logging.info("\n Retrieving Pages")
 
-    info = pdfinfo_from_path(path, userpw=None, poppler_path=None)
+    pdf_info = pdfinfo_from_path(pdfpath, userpw=None, poppler_path=None)
 
-    maxPages = info["Pages"]
-
-    n = 1
+    max_pages = pdf_info["Pages"]
 
     # Create temporary directory for image files.
-    os.mkdir("data/temp_pdf_to_jpg")
+    TEMP_FOLDER = os.path.join( settings.DATA_FOLDER, 'TEMP' )
+    _soft_mkdir( TEMP_FOLDER )
 
-    for page in range(1, maxPages+1, 10) :
-        pages = convert_from_path(path, 
-                              dpi=300, 
-                              first_page=page, 
-                              last_page = min(page+10-1,maxPages),
-                              thread_count = 10)
+    # subfolder for specific year's logs as jpg files
+    TEMP_JPG_FOLDER = os.path.join( TEMP_FOLDER, "{}_jpg_logs".format(year) )
+    _soft_mkdir( TEMP_JPG_FOLDER )
+
+    # subfolder for specific year's text files.
+    _soft_mkdir( settings.TEXT_OUTPUT_FOLDER )
+    TEMP_TXT_FOLDER = os.path.join( settings.TEXT_OUTPUT_FOLDER, "{}_text_logs".format(year) )
+    _soft_mkdir( TEMP_TXT_FOLDER )
+    
+    
+
+    # TODO: consider moving these to optional arguments of the function and/or argv[1], sys.argv[2]
+    FIRST_PAGE = 1
+    LAST_PAGE = 10   # use max_pages for entire pdf file's logs.
+    for pagenum in range(FIRST_PAGE, LAST_PAGE+1, settings.BATCHSIZE):
+        BATCHSIZE = settings.BATCHSIZE
+        pages = convert_from_path(pdfpath, 
+                              dpi = settings.DPI, 
+                              first_page = pagenum, 
+                              last_page = min(pagenum + settings.BATCHSIZE - 1, LAST_PAGE),
+                              thread_count = settings.MAXTHREADS)
             
 
-        logging.info("\n Retrieved Pages {} to {}.".format(n, n + len(pages)))
+        logging.info("\n Retrieved Pages {} to {}.".format(pagenum, pagenum + len(pages) - 1))
 
-        for page in pages:
+        for i,page in enumerate(pages):
             # Write pdf to image file.
-            img_file = 'data/{}_jpg_logs/out_{}.jpg'.format(year,n)
-            page.save(img_file.format(n),"JPEG")
+            IMG_FILENAME = os.path.join( TEMP_JPG_FOLDER, "out_{}.jpg".format(pagenum+i) )
+            page.save(IMG_FILENAME, "JPEG")
+            logging.log(VERBOSE_LOGLEVEL, "Image written: {}".format(os.path.basename(IMG_FILENAME)))
 
 
             # Write image file to text file.
-            text_file = open('data/{}_text_logs/page_{}.txt'.format(year,n), 'w')
-            text=str(pytesseract.image_to_string(Image.open(img_file),lang='eng', config='--psm 12'))  # 12
+            TXT_FILENAME = os.path.join( TEMP_TXT_FOLDER, "page_{}.txt".format(pagenum+i) )
+            text_file = open( TXT_FILENAME, 'w')
+            
+            text=str(pytesseract.image_to_string(Image.open(IMG_FILENAME), lang='eng', config='--psm 12'))  # 12
+            
+            # TODO: look over/validate this choice.
             text=text.replace("\n"," ") # make spaces instead of lose info
 
             text_file.write(text)
+            logging.log(VERBOSE_LOGLEVEL, "Text written: {}".format(os.path.basename(TXT_FILENAME)))
+            
             text_file.close()
             
-            
             #and delete image file
-            os.remove(img_file)
-
-            n += 1
-
+            if settings.CLEAN_TEMP_FILES:
+                os.remove(IMG_FILENAME)
+                logging.log(VERBOSE_LOGLEVEL, "Image deleted: {}".format(os.path.basename(IMG_FILENAME)))
+        # end for
+    # end for
+    
+    # Finished.
     # Delete temporary directory for image files.
-    os.rmdir("data/pdf_to_jpg")
+    if settings.CLEAN_TEMP_FILES:
+        # if there is any other content in the same TEMP_JPG_FOLDER, then *don't* delete everything;
+        # only the tmep files related to the file for the current call to the function.
+        if len( settings.glob.glob( os.path.join(TEMP_FOLDER, '*') ) ) > 1:
+            logging.info("Other folders in {}; only deleted relevant temp data for this run.".format(TEMP_JPG_FOLDER))
+            os.rmdir( TEMP_JPG_FOLDER )
+            logging.log(VERBOSE_LOGLEVEL, "Folder deleted: {}".format(TEMP_JPG_FOLDER))
+        else:
+            # delete all temp data.
+            os.rmdir( TEMP_JPG_FOLDER )
+            logging.log(VERBOSE_LOGLEVEL, "Folder deleted: {}".format(TEMP_JPG_FOLDER) )
+            os.rmdir( TEMP_FOLDER )
+            logging.log(VERBOSE_LOGLEVEL, "Folder deleted: {}".format(TEMP_FOLDER) )
+    
+    logging.info("Finished processing with inputs {}".format(sys.argv[1:]))
 
 if __name__ == "__main__":
     main(sys.argv[1:])
---
+++ settings.py
@@ -0,0 +1,48 @@
+import os   # build folder locations independent of operating system
+import glob # used to search for file(s) using wildcard *
+import sys
+
+import pytesseract
+
+# whether to delete temporary files generated during the OCR process.
+CLEAN_TEMP_FILES = True
+BATCHSIZE = 4
+MAXTHREADS = 4
+DPI = 300
+
+# Define a tesseract location (OCR tool needs to be installed separately from pytesseract -- at least on Windows).
+# If the executable that pytesseract is trying to use doesn't exist, 
+if os.access( pytesseract.pytesseract.tesseract_cmd, os.F_OK ):
+    # then manually type the location here.
+    TESSERACT_CMD = "C:\\Users\\maminian\\AppData\\Local\\Programs\\Tesseract-OCR\\tesseract.exe"
+    pytesseract.pytesseract.tesseract_cmd = TESSERACT_EXECUTABLE
+
+# Strings of absolute folder locations for the entire project.
+# If you clone the repository, these match that structure, and nothing needs to be done.
+PROJECT_FOLDER = os.path.dirname( os.path.realpath(__file__) )          # get full path of this script
+DATA_FOLDER = os.path.join(PROJECT_FOLDER, 'data')
+PDF_FOLDER = os.path.join(DATA_FOLDER, 'primary_datasets') # ./data/primary_datasets/
+CODE_FOLDER = os.path.join(PROJECT_FOLDER, 'code')  # ./code/
+
+PDF_FOLDER = os.path.abspath(PDF_FOLDER)
+CODE_FOLDER = os.path.abspath(CODE_FOLDER)
+
+PDF_FILE_SEARCH = os.path.join(PDF_FOLDER, '*.pdf')
+PDF_FILES = glob.glob(PDF_FILE_SEARCH)
+
+#
+TEXT_OUTPUT_FOLDER = os.path.join( DATA_FOLDER, 'text_logs' )
+
+# make dictionary which has shortened names for the pdf files.
+# ex: Logs2019.pdf maps to its full location in the operating system.
+PDF_DICT = {os.path.basename(fname) : fname for fname in PDF_FILES}
+
+# A couple manually defined entries for backwards compatibility.
+try:
+    PDF_DICT['2019'] = PDF_DICT['Logs2019.pdf']
+    PDF_DICT['2020'] = PDF_DICT['Logs2020.pdf']
+except:
+    # Should get error only if Logs2019.pdf and Logs2020.pdf are not entries in PDF_DICT.
+    # Probably only happens if the directory structure was changed.
+    pass
+    

6269ff4ea7477fd7767fdfde0d67492295903e43
mendible <ariana.mendible@gmail.com>
2022-03-03 10:52:49 -0800

    again trying to merge result

--- settings.py
+++ settings.py
@@ -3,6 +3,7 @@ import glob # used to search for file(s) using wildcard *
 import sys
 
 import pytesseract
+from numpy import ones,uint8
 
 # whether to delete temporary files generated during the OCR process.
 CLEAN_TEMP_FILES = True
@@ -10,12 +11,27 @@ BATCHSIZE = 4
 MAXTHREADS = 4
 DPI = 400
 
+# image processing parameters
+KERNEL = ones((3,3), uint8) # kernel for image denoising
+THRESH = 150 # thresholding for binary image, 0-255
+
+# ocr tuning parameters
+N_LINES = 66 # total number of lines to be read in the document for clustering
+LINE_LABELS = list(range(N_LINES)) # labels for line number
+BLACKLIST = ";{}&£~=%¥€" # characters we want to specifically exclude from the OCR
+
 # If you've installed tesseract manually, then you'll need to
 # manually define the path to the executable here.
 # By default, pytesseract assumes you can access this with the
 # command "tesseract" in the command line.
-TESSERACT_CMD = ""
+TESSERACT_CMD = "/usr/local/bin/tesseract"
 
+#Pyesseract config string
+CONFIG = '-c tessedit_char_blacklist={} \
+        --psm 1 \
+        --dpi {} \
+        --user-patterns eng.my-patterns \
+        --user-words eng.my-words'.format(BLACKLIST,DPI)
 
 # Specify tesseract location for pytesseract if its default
 # location doesn't exist.
@@ -38,9 +54,6 @@ CODE_FOLDER = os.path.abspath(CODE_FOLDER)
 
 PDF_FILE_SEARCH = os.path.join(PDF_FOLDER, '*.pdf')
 PDF_FILES = glob.glob(PDF_FILE_SEARCH)
-
-#
-TEXT_OUTPUT_FOLDER = os.path.join( DATA_FOLDER, 'text_logs' )
 
 # make dictionary which has shortened names for the pdf files.
 # ex: Logs2019.pdf maps to its full location in the operating system.

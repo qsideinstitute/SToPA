960b7ee8625a94d3cce5bee62a50f0ca1d22873a
Manuchehr Aminian <manuchehr.aminian@gmail.com>
2022-06-22 08:45:28 -0700

    pytesseract import optional

--- code/settings.py
+++ code/settings.py
@@ -2,40 +2,7 @@ import os   # build folder locations independent of operating system
 import glob # used to search for file(s) using wildcard *
 import sys
 import shutil
-
-import pytesseract
-from numpy import ones,uint8
-
-# whether to delete temporary files generated during the OCR process.
-CLEAN_TEMP_FILES = True
-BATCHSIZE = 4
-MAXTHREADS = 4
-DPI = 400
-
-# image processing parameters
-KERNEL = ones((3,3), uint8) # kernel for image denoising
-THRESH = 150 # thresholding for binary image, 0-255
-
-# ocr tuning parameters
-N_LINES = 66 # total number of lines to be read in the document for clustering
-LINE_LABELS = list(range(N_LINES)) # labels for line number
-BLACKLIST = ";{}&£~=%¥€" # characters we want to specifically exclude from the OCR
-
-#Pyesseract config string
-CONFIG = '-c tessedit_char_blacklist={} \
-        --psm 1 \
-        --dpi {} \
-        --user-patterns eng.my-patterns \
-        --user-words eng.my-words'.format(BLACKLIST,DPI)
-
-# Is the default "tesseract_cmd" an executable that the operating system recognizes?
-_is_tesseract_executable = os.access(shutil.which(pytesseract.pytesseract.tesseract_cmd), os.X_OK)
-if _is_tesseract_executable:
-    # If yes, then set as executible command
-    TESSERACT_CMD = pytesseract.pytesseract.tesseract_cmd
-else:
-    # If no, then repalce with full path to the tesseract binary.
-    TESSERACT_CMD = shutil.which(pytesseract.pytesseract.tesseract_cmd)
+import warnings
 
 # Strings of absolute folder locations for the entire project.
 # If you clone the repository, these match that structure, and nothing needs to be done.
@@ -50,3 +17,53 @@ PDF_FILES = glob.glob(PDF_FILE_SEARCH)
 # make dictionary which has shortened names for the pdf files.
 # ex: Logs2019.pdf maps to its full location in the operating system.
 PDF_DICT = {os.path.basename(fname) : fname for fname in PDF_FILES}
+
+# 
+# Pytesseract settings below.
+
+try:
+    import pytesseract
+    _pytesseract_imported = True
+except:
+    _pytesseract_imported = False
+    warnings.warn(
+        "Importing pytesseract failed. OCR capabilities are disabled."
+        )
+#import pytesseract
+
+if _pytesseract_imported:
+    from numpy import ones,uint8
+    
+    # whether to delete temporary files generated during the OCR process.
+    CLEAN_TEMP_FILES = True
+    BATCHSIZE = 4
+    MAXTHREADS = 4
+    DPI = 400
+    
+    # image processing parameters
+    KERNEL = ones((3,3), uint8) # kernel for image denoising
+    THRESH = 150 # thresholding for binary image, 0-255
+    
+    # ocr tuning parameters
+    N_LINES = 66 # total number of lines to be read in the document for clustering
+    LINE_LABELS = list(range(N_LINES)) # labels for line number
+    BLACKLIST = ";{}&£~=%¥€" # characters we want to specifically exclude from the OCR
+    
+    #Pyesseract config string
+    # TODO: where is this CONFIG variable used?
+    # If used, can we rename the variable to something
+    # more specific (ex: PYTESSERACT_CONFIG)
+    CONFIG = '-c tessedit_char_blacklist={} \
+            --psm 1 \
+            --dpi {} \
+            --user-patterns eng.my-patterns \
+            --user-words eng.my-words'.format(BLACKLIST,DPI)
+    
+    # Is the default "tesseract_cmd" an executable that the operating system recognizes?
+    _is_tesseract_executable = os.access(shutil.which(pytesseract.pytesseract.tesseract_cmd), os.X_OK)
+    if _is_tesseract_executable:
+        # If yes, then set as executible command
+        TESSERACT_CMD = pytesseract.pytesseract.tesseract_cmd
+    else:
+        # If no, then repalce with full path to the tesseract binary.
+        TESSERACT_CMD = shutil.which(pytesseract.pytesseract.tesseract_cmd)

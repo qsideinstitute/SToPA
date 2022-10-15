b4e136f5df6c12a29f2c754f347787bc3e2c179b
Spencer Brooks <spencer.brooks0119@gmail.com>
2022-10-15 02:29:01 -0400

    Modularized some of gen_leaflet

--- analysis/interactive_zoomable_map/gen_leaflet.py
+++ analysis/interactive_zoomable_map/gen_leaflet.py
@@ -2,79 +2,126 @@ import pandas as pd
 import numpy as np
 import datetime
 
-df = pd.read_csv("df_with_geo_data.csv")
+### Interface ###
 
-output = ""
+class MapWriter():
 
-selector_name = "call-reason"
-selector_name2 = "call-taker"
+    slots = ("_df_origin", "_target_path", "_df", "_output", "_selector_names", "_coords", "_init_zoom")
 
-all_option_name = "All reasons"
-all_option_name2 = "All call takers"
+    
+    @staticmethod
+    def html_friendly_name(s):
+        return s.replace("_", "-")
 
-with open("map_template.html",'r') as f:
-    lines = f.readlines()
-    for line in lines:
-        output += line
-        if (line == "  <!--SELECTORS-->\n"):
-            output += "<select name=\"" + selector_name + "\" class=\"custom-select\" id=\""+selector_name+"\" onchange=\"updateMap()\">\n<option value=\""+all_option_name+"\">"+all_option_name+"</option>\n"
+    
+    def __init__(self):
+        self._df_origin = "./df_with_geo_data.csv"
+        self._template_path = "./map_template.html"
+        self._target_path = "./map_out.html"
+        self._df = pd.read_csv(self._df_origin)
+        self._selector_names = {"call_reason": "All reasons", "call_taker": "All call takers", "street": "All streets"}
+        self._coords = [42.7, -73.2]
+        self._init_zoom = 12
 
-            list_of_reasons = list(set(df['call_reason']))
+
+    def selector_html(self):
+        result = ""
+        for selection_box in self._selector_names:
+            result += "<select name=\"" + MapWriter.html_friendly_name(selection_box) + "\" class=\"custom-select\" id=\""+ MapWriter.html_friendly_name(selection_box) +"\" onchange=\"updateMap()\">\n<option value=\"" + self._selector_names[selection_box]  + "\">" + self._selector_names[selection_box] + "</option>\n"
+            list_of_reasons = list(set(self._df[selection_box]))
             list_of_reasons.sort()
             for reason in list_of_reasons:
-                output += "<option value=\"" + reason + "\">" + reason + "</option>\n"
-            output += "</select>"
+                result += "<option value=\"" + reason + "\">" + reason + "</option>\n"
+            result += "</select>\n"
+        return result
 
-            output += "<select name=\"" + selector_name2 + "\" class=\"custom-select\" id=\""+selector_name2+"\" onchange=\"updateMap()\">\n<option value=\""+all_option_name2+"\">"+all_option_name2+"</option>\n"
-            list_of_officers = list(set(df['call_taker']))
-            list_of_officers.sort()
-            for officer in list_of_officers:
-                output += "<option value=\"" + officer + "\">" + officer + "</option>\n"
-            output += "</select>"
+
+    def write_template_to_html(self):
+        self._output = ""
+        with open(self._template_path,'r') as f:
+            lines = f.readlines()
+            for line in lines:
+                self._output += line
+                if (line == "  <!--SELECTORS-->\n"):
+                    self._output += self.selector_html()
+                elif (line == "  //MAP\n"):
+                    self._output += self.map_html()
+                elif (line == "  //FUNCTIONS\n"):
+                    self._output += self.functions()
+        with open(self._target_path, 'w') as f:
+            f.write(self._output)
+
             
+    def functions(self):
+        result = ""
+        result += "function currentAttributes() {\n"
+        result += "var result = new Map();\n"
+        for selection_box in self._selector_names:
+            result += "result.set(\"" + MapWriter.html_friendly_name(selection_box) + "\", $(\'#" + MapWriter.html_friendly_name(selection_box) + "\').val());\n"
+        result += "return result;\n"
+        result += "}\n"
+
+        result += "function updateMap() {\n"
+        result += "attributes = currentAttributes();\n"
+        result += "markers.forEach(function(key, marker) {\n"
+        result += "if (!marker_cluster.hasLayer(marker)) {\n"
+        result += "marker.addTo(marker_cluster);\n"
+        result += "}"
+        for selection_box in self._selector_names:
+            result += "if (marker.attributes[\"" + MapWriter.html_friendly_name(selection_box) + "\"] != attributes.get(\"" + MapWriter.html_friendly_name(selection_box) + "\") && attributes.get(\"" + MapWriter.html_friendly_name(selection_box) + "\") != \"" + self._selector_names[selection_box] + "\") {\n"
+            result += "marker_cluster.removeLayer(marker);\n"
+            result += "}\n"
+        result += "});\n}\n"
+
+        return result
+
+    
+    def map_html(self):
+        result = ""
+        result += "var map = L.map(\"map\").setView(" + str(self._coords) + ", " + str(self._init_zoom) + ")\n"
+        result += "var tileLayer = L.tileLayer(\'https://tile.openstreetmap.org/{z}/{x}/{y}.png\', {\nmaxZoom: 18, attribution: \'&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>\'\n}).addTo(map);\n"
+        result += "var marker_cluster = L.markerClusterGroup({});\nmap.addLayer(marker_cluster);"
+        result += "var myIcon = L.AwesomeMarkers.icon({\"extraClasses\": \"fa-rotate-0\", \"icon\": \"building\", \"iconColor\": \"white\", \"markerColor\": \"black\", \"prefix\": \"fa\"});\n" # TODO implement icon options
+        result += "let markers = new Map();\n"
+
+        for lab, row in self._df.iterrows():
+            marker_coords = list(row[['latitude','longitude']])
+            if False in [marker_coords[i] == marker_coords[i] for i in range(len(marker_coords))]:
+                continue
+            str_coords = str(marker_coords)
+            marker_label = "marker" + str(lab)
+            popup_label = "popup" + str(lab)
+            html_label = "popupHtml" + str(lab)
+
+            ### Data ###
+            # TODO modularize, abstract, implement option to format popups and other displays
+            pdf_page = row['pdf_page']
+            street = row['street']
+            date_time_object = datetime.datetime.strptime(row["call_datetime"], "%Y-%m-%d %H:%M:%S")if row["call_datetime"] == row["call_datetime"] else None
+            date_time = date_time_object.strftime("%m/%d/%Y, %I:%M%p").lower() if not date_time_object is None else ""
+            year = date_time_object.strftime("%Y") if not date_time_object is None else "No date/time found."
+            call_reason = row['call_reason']
+            call_taker = row['call_taker']
+            narrative = row['narrative'].replace("\"", "\\\"") if row['narrative'] == row['narrative'] else ""
+            py_dict = {"pdf_page": pdf_page, "street": street, "date_time": date_time, "call-reason": call_reason, "narrative": narrative, "call-taker": call_taker}
                 
-        elif (line == "  //MAP\n"):
-            map_coords = [42.7, -73.2]
-            init_zoom = 12
-            output += "var map = L.map(\"map\").setView(" + str(map_coords) + ", " + str(init_zoom) + ")\n"
-            output += "var tileLayer = L.tileLayer(\'https://tile.openstreetmap.org/{z}/{x}/{y}.png\', {\nmaxZoom: 18, attribution: \'&copy; <a href=\"http://www.openstreetmap.org/copyright\">OpenStreetMap</a>\'\n}).addTo(map);\n"
-            output += "var marker_cluster = L.markerClusterGroup({});\nmap.addLayer(marker_cluster);"
+            result += "var " + marker_label + " = L.marker(" + str_coords + ",{icon: myIcon});\n"
+            result += marker_label + ".attributes = {" + ", ".join(["\"" + str(key) + "\": \"" + str(value) + "\"" for key, value in zip(py_dict.keys(), py_dict.values())])+ "}\n"
+            result += "var " + popup_label + " = L.popup({\"maxWidth\": 400, \"minWidth\": 400});\n"
+            result += "var " + html_label + " = $(`<div id=\"" + html_label + "\" style=\"width: 100.0%; height: 100.0%;\"><a href=../../data/primary_datasets/Logs{}.pdf#page={} target=\"blank\" rel=\"noopener noreferrer\">Log No. {}</a><br>{}<br>{}<br>Call Reason: {}<br>Call Taker: {}<br>Narrative:<br>{}</div>`)[0];\n".format(year, str(pdf_page), str(row['log_num']), street, date_time, call_reason, call_taker, narrative)
+            result += popup_label + ".setContent(" + html_label + ");\n"
+            result += marker_label + ".bindPopup(" + popup_label + ");\n"
+            result += marker_label + ".bindTooltip(\n`<div>" + street + "</div>`,{\"sticky\": true});\n"
+            result += marker_label + ".addTo(marker_cluster);\n"
 
-            output += "var myIcon = L.AwesomeMarkers.icon({\"extraClasses\": \"fa-rotate-0\", \"icon\": \"building\", \"iconColor\": \"white\", \"markerColor\": \"black\", \"prefix\": \"fa\"});\n"
+            result += "markers.set(" + marker_label + ", " + str(lab) + ");\n"
 
-            output += "let markers = new Map();\n"
+        result += "var attributes\n"
+        result += "updateMap()\n"
 
-            for lab, row in df.iterrows():
-                marker_coords = list(row[['latitude','longitude']])
-                if False in [marker_coords[i] == marker_coords[i] for i in range(len(marker_coords))]:
-                    continue
-                str_coords = str(marker_coords)
-                marker_label = "marker" + str(lab)
-                popup_label = "popup" + str(lab)
-                html_label = "popupHtml" + str(lab)
+        return result
 
-                ### Data ###
-                pdf_page = row['pdf_page']
-                street = row['street']
-                date_time_object = datetime.datetime.strptime(row["call_datetime"], "%Y-%m-%d %H:%M:%S")if row["call_datetime"] == row["call_datetime"] else None
-                date_time = date_time_object.strftime("%m/%d/%Y, %I:%M%p").lower() if not date_time_object is None else ""
-                year = date_time_object.strftime("%Y") if not date_time_object is None else "No date/time found."
-                call_reason = row['call_reason']
-                call_taker = row['call_taker']
-                narrative = row['narrative'].replace("\"", "\\\"") if row['narrative'] == row['narrative'] else ""
-                py_dict = {"pdf_page": pdf_page, "street": street, "date_time": date_time, "call-reason": call_reason, "narrative": narrative, "call-taker": call_taker}
-                
-                output += "var " + marker_label + " = L.marker(" + str_coords + ",{icon: myIcon});\n"
-                output += marker_label + ".attributes = {" + ", ".join(["\"" + str(key) + "\": \"" + str(value) + "\"" for key, value in zip(py_dict.keys(), py_dict.values())])+ "}\n"
-                output += "var " + popup_label + " = L.popup({\"maxWidth\": 400, \"minWidth\": 400});\n"
-                output += "var " + html_label + " = $(`<div id=\"" + html_label + "\" style=\"width: 100.0%; height: 100.0%;\"><a href=../../data/primary_datasets/Logs{}.pdf#page={} target=\"blank\" rel=\"noopener noreferrer\">Log No. {}</a><br>{}<br>{}<br>Call Reason: {}<br>Call Taker: {}<br>Narrative:<br>{}</div>`)[0];\n".format(year, str(pdf_page), str(row['log_num']), street, date_time, call_reason, call_taker, narrative)
-                output += popup_label + ".setContent(" + html_label + ");\n"
-                output += marker_label + ".bindPopup(" + popup_label + ");\n"
-                output += marker_label + ".bindTooltip(\n`<div>" + street + "</div>`,{\"sticky\": true});\n"
-                output += marker_label + ".addTo(marker_cluster);\n"
 
-                output += "markers.set(" + marker_label + ", " + str(lab) + ");\n"
-                
-
-with open("map_out.html", 'w') as f:
-    f.write(output)
+if __name__ == "__main__":
+    mw = MapWriter()
+    mw.write_template_to_html()
